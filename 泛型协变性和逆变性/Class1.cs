668397c91165e7f090420a1c91f4a3629d1b4b4b
刘承志 <632473061@qq.com>
2017-12-26 18:14:55 +0800

    12

---
+++ 泛型协变性和逆变性/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 泛型协变性和逆变性
+{
+    class Class1
+    {
+    }
+}
